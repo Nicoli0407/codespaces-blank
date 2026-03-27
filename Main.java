8aeac81bad5e496756d39504edf96219c164859c
Nicoli0407 <nicoliss040712@gmail.com>
2026-03-27 19:00:59 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+     System.out.println("Olá turma!");
+     System.out.println("Estou usando o Java no GitHub Codespaces.");
+     }
+
+}
